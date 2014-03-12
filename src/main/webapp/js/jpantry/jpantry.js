7263ecfc91e8707654a03b5e23775668de3304f4
unknown <u23773@BCH200-E21.dtenet.com>
2014-03-12 12:21:02 -0400

    Adding the calls to the new intake and outbound APIs

--- src/main/webapp/js/jpantry/jpantry.js
+++ src/main/webapp/js/jpantry/jpantry.js
@@ -1,18 +1,37 @@
 var jPantry = angular.module('jPantry', ['ngResource', 'jpantryLookUpService']);
 
-jPantry.controller('jPantryCntrl', ['$scope', 'jPantryService', function($scope, jPantryService) {
+jPantry.controller('jPantryCntrl', ['$scope', 'jPantryService', 'jPantryItemIntake', 'jPantryItemOutbound', 
+function($scope, jPantryService, jPantryItemIntake, jPantryItemOutbound) {
+	
+	var quantityTextField = angular.element('#quantityTextField');
 	
 	$scope.upcCodeKeyPress = function(event) {
 		if (event.which==13) {
-    		angular.element(document.getElementById('quantityTextField')).focus();
+    		if($scope.scanType == 'lookup') {
+				$scope.item = jPantryService.get({upcCode: $scope.upcCode});
+			} else {
+				quantityTextField.focus();
+			}
     	}
 	}
 	
 	$scope.quantityKeyPress = function(event) {
 		if (event.which==13) {
-			$scope.item = jPantryService.get({upcCode: $scope.upcCode});
+			if($scope.scanType == 'in') {
+				$scope.item = jPantryItemIntake.get({upcCode: $scope.upcCode, quantity: $scope.quantity});
+			} else if($scope.scanType == 'out') {
+				$scope.item = jPantryItemOutbound.get({upcCode: $scope.upcCode, quantity: $scope.quantity});
+			}
 		}
 	}
+	
+	$scope.$watch('scanType', function(scanType) {
+       if(scanType === 'lookup') {
+			quantityTextField.parent().hide();
+	   } else {
+			quantityTextField.parent().show();
+	   }
+	});
 			
 }]);
 
@@ -20,4 +39,12 @@ var jpantryLookUpService = angular.module('jpantryLookUpService', ['ngResource']
 
 jpantryLookUpService.factory('jPantryService', ['$resource', function($resource) {
 	return $resource('/JPantry/rest/item/find/:upcCode', {upcCode: '@upcCode'});
+}]);
+
+jpantryLookUpService.factory('jPantryItemIntake', ['$resource', function($resource) {
+	return $resource('/JPantry/rest/item/in/:upcCode/:quantity', {upcCode: '@upcCode', quantity: '@quantity'});
+}]);
+
+jpantryLookUpService.factory('jPantryItemOutbound', ['$resource', function($resource) {
+	return $resource('/JPantry/rest/item/out/:upcCode/:quantity', {upcCode: '@upcCode', quantity: '@quantity'});
 }]);
