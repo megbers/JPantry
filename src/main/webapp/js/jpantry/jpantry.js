4d3ae1aa9c8e64422eb3cb0b98dd99a50161fd65
Matt Egbers <matt.egbers@contractor.cengage.com>
2014-03-15 23:22:13 -0400

    Adding a lot of GUI stuff.  Started using pure css to clean up the UI

--- src/main/webapp/js/jpantry/jpantry.js
+++ src/main/webapp/js/jpantry/jpantry.js
@@ -1,17 +1,29 @@
 var jPantry = angular.module('jPantry', ['ngResource', 'jPantryService']);
 
-jPantry.controller('jPantryCntrl', ['$scope', 'jPantryLookUpService', 'jPantryItemIntake', 'jPantryItemOutbound', 
-function($scope, jPantryLookUpService, jPantryItemIntake, jPantryItemOutbound) {
+jPantry.controller('jPantryCntrl', ['$scope', '$rootScope', 'jPantryLookUpService', 'jPantryItemIntake', 'jPantryItemOutbound',
+function($scope, $rootScope, jPantryLookUpService, jPantryItemIntake, jPantryItemOutbound) {
 	var upcCodeTextField = angular.element('#upcCodeTextField');
 	var quantityTextField = angular.element('#quantityTextField');
 	var onListCheckbox = angular.element('#onListCheckbox');
+
+    var allList = angular.element('#jPantryAllListCntrl');
+    var shoppingList = angular.element('#jPantryShoppingListCntrl');
+    var inventoryList = angular.element('#jPantryInventoryListCntrl');
+
 	$scope.scanType = 'lookup';
-	
+
+    function resetFields() {
+        $scope.upcCode = '';
+        $scope.quantity = '';
+    }
+
 	$scope.upcCodeKeyPress = function(event) {
 		if (event.which == 13) {
     		if($scope.scanType == 'lookup') {
-				$scope.item = jPantryLookUpService.get({upcCode: $scope.upcCode});
-				$scope.upcCode = '';
+				$scope.item = jPantryLookUpService.get({upcCode: $scope.upcCode}, function() {
+                    $rootScope.$broadcast('refreshAll', {});
+                });
+                resetFields();
 			} else {
 				quantityTextField.focus();
 			}
@@ -21,28 +33,44 @@ function($scope, jPantryLookUpService, jPantryItemIntake, jPantryItemOutbound) {
 	$scope.quantityKeyPress = function(event) {
 		if (event.which == 13) {
 			if($scope.scanType == 'in') {
-				$scope.item = jPantryItemIntake.get({upcCode: $scope.upcCode, quantity: $scope.quantity});
+				$scope.item = jPantryItemIntake.get({upcCode: $scope.upcCode, quantity: $scope.quantity}, function() {
+                    $rootScope.$broadcast('refreshInventory', {});
+                    $rootScope.$broadcast('refreshAll', {});
+                });
 			} else if($scope.scanType == 'out') {
-				$scope.item = jPantryItemOutbound.get({upcCode: $scope.upcCode, quantity: $scope.quantity, onList: $scope.onList});
+                var onList = $scope.onList ? $scope.onList : false;
+				$scope.item = jPantryItemOutbound.get({upcCode: $scope.upcCode, quantity: $scope.quantity, onList: onList}, function() {
+                    $rootScope.$broadcast('refreshShopping', {});
+                    $rootScope.$broadcast('refreshAll', {});
+                });
 			}
-			$scope.upcCode = '';
-			$scope.quantity = '';
+            resetFields();
 			upcCodeTextField.focus();
 		}
 	}
-	
-	$scope.$watch('scanType', function(scanType) {
+
+    $scope.$watch('scanType', function(scanType) {
+       allList.hide();
+       shoppingList.hide();
+       inventoryList.hide();
+
        if(scanType === 'lookup') {
-			quantityTextField.parent().hide();
+           quantityTextField.parent().hide();
+           allList.show();
 	   } else {
-			quantityTextField.parent().show();
+           quantityTextField.parent().show();
 	   }
 	   
 	   if(scanType === 'out') {
-	   		onListCheckbox.parent().show();
+	       onListCheckbox.parent().show();
+           shoppingList.show();
 	   } else {
 	   		onListCheckbox.parent().hide();
 	   }
+
+       if(scanType === 'in') {
+           inventoryList.show();
+       }
 	});
 			
 }]);
@@ -50,19 +78,43 @@ function($scope, jPantryLookUpService, jPantryItemIntake, jPantryItemOutbound) {
 
 
 
-jPantry.controller('jPantryInventoryListCntrl', ['$scope', 'jPantryInventoryListService',
-function($scope, jPantryInventoryListService) {
-	$scope.inventoryList = jPantryInventoryListService.query({});
+jPantry.controller('jPantryInventoryListCntrl', ['$scope', 'jPantryInventoryListService', 'jPantryItemIntake', 
+function($scope, jPantryInventoryListService, jPantryItemIntake) {
+    $scope.$on('refreshInventory', function(event, data) {
+        refreshInventory();
+    });
+
+    function refreshInventory() {
+        $scope.inventoryList = jPantryInventoryListService.query({});
+    }
+
+    refreshInventory();
 }]);
 
 jPantry.controller('jPantryShoppingListCntrl', ['$scope', 'jPantryShoppingListService',
 function($scope, jPantryShoppingListService) {
-	$scope.shoppingList = jPantryShoppingListService.query({});
+    $scope.$on('refreshShopping', function(event, data) {
+        refreshShopping();
+    });
+
+    function refreshShopping() {
+        $scope.shoppingList = jPantryShoppingListService.query({});
+    }
+
+    refreshShopping()
 }]);
 
 jPantry.controller('jPantryAllListCntrl', ['$scope', 'jPantryAllListService',
 function($scope, jPantryAllListService) {
-	$scope.allList = jPantryAllListService.query({});
+    $scope.$on('refreshAll', function(event, data) {
+        refreshAll();
+    });
+
+    function refreshAll() {
+        $scope.allList = jPantryAllListService.query({});
+    }
+
+    refreshAll();
 }]);
 
 
